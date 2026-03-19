09d0224d8f0dc6c8ddea118835f908e76985570d
Islam-HH <islamhesham196@outlook.com>
2026-03-19 05:43:18 +0200

    updated the media queries

--- main.js
+++ main.js
@@ -3,3 +3,4 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '@fontsource/red-rose/400.css'
 import '@fontsource/sansation/700.css'
+import '@fontsource/sarabun/500.css'

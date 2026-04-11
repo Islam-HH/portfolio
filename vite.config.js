af20df0d845cbf3e35c2c53769c6054f8df0ef88
Islam-HH <islamhesham196@outlook.com>
2026-04-11 02:39:40 +0200

    fixed the name

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // Set the base to your repository name surrounded by slashes
-  base: '/-portfolio/',
+  base: '/portfolio/',
 })
